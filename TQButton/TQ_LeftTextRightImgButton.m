14798e197b5ebb72ebc5886a420edd6242929f6b
litianqi <litianqi@joyy.com>
2024-01-25 19:02:12 +0800

    fix: 代码缺失

--- TQButton/TQ_LeftTextRightImgButton.m
+++ TQButton/TQ_LeftTextRightImgButton.m
@@ -12,18 +12,45 @@
 
 - (void)layoutSubviews
 {
-     [super layoutSubviews];
- 
+    [super layoutSubviews];
+    
     CGFloat imageWidth = 0;
     CGFloat titleWidth = 0;
     if (self.currentImage) {
         imageWidth = self.currentImage.size.width + _leftForText;
-        titleWidth = [self.currentTitle hq_getWidthWithFont:self.titleLabel.font constrainedToSize:CGSizeMake(CGFLOAT_MAX, 30)] + _rightFotImg;
+        titleWidth = [self.currentTitle tq_getWidthWithFont:self.titleLabel.font constrainedToSize:CGSizeMake(CGFLOAT_MAX, 30)] + _rightFotImg;
         [self setTitleEdgeInsets:UIEdgeInsetsMake(0, -imageWidth, 0, imageWidth)];
         [self setImageEdgeInsets:UIEdgeInsetsMake(0, titleWidth, 0, -titleWidth)];
     }
-  
- }
+    
+}
+
+- (CGFloat)tq_getWidthWithFont:(UIFont *)font constrainedToSize:(CGSize)size
+{
+    return [self tq_getSizeWithFont:font constrainedToSize:size].width;
+}
+
+- (CGSize)tq_getSizeWithFont:(UIFont *)font paragraphStyle:(NSMutableParagraphStyle *)paragraphStyle constrainedToSize:(CGSize)size
+{
+    CGSize resultSize = CGSizeZero;
+    if (self.length <= 0) {
+        return resultSize;
+    }
+    if (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_6_1) {
+        resultSize = [self boundingRectWithSize:size
+                                        options:(NSStringDrawingUsesLineFragmentOrigin |NSStringDrawingUsesFontLeading)
+                                     attributes:@{NSFontAttributeName: font,NSParagraphStyleAttributeName:paragraphStyle}
+                                        context:nil].size;
+    } else {
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_7_0
+        resultSize = [self sizeWithFont:font constrainedToSize:size lineBreakMode:NSLineBreakByWordWrapping];
+        
+#endif
+    }
+    resultSize = CGSizeMake(MIN(size.width, ceilf(resultSize.width)), MIN(size.height, ceilf(resultSize.height)));
+    return resultSize;
+}
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
